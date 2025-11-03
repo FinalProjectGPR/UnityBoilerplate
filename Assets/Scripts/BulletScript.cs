536f89c01fc4d94d7fac1377686231dd543096f3
Tucker <tucker.beard@mymail.champlain.edu>
2025-11-03 17:58:41 -0500

    Finished Gun Script

--- Assets/Scripts/BulletScript.cs
+++ Assets/Scripts/BulletScript.cs
@@ -6,15 +6,34 @@ public class BulletScript : MonoBehaviour
 {
     public Rigidbody2D rb;
     public int direction = 1;
+
+    public float speed;
+    public float aliveTime;
+
     // Start is called before the first frame update
     void Start()
     {
         rb = gameObject.GetComponent<Rigidbody2D>();
+        rb.velocity = new Vector3(Mathf.Cos(transform.localEulerAngles.z * Mathf.Deg2Rad) * speed, Mathf.Sin(transform.localEulerAngles.z * Mathf.Deg2Rad) * speed, 0f);
     }
 
     // Update is called once per frame
     void Update()
     {
-        rb.velocity = new Vector3(5f * direction, 0f, 0f);
+        aliveTime -= Time.deltaTime;
+        if(aliveTime <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Destroy(gameObject);
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        gameObject.GetComponent<CircleCollider2D>().isTrigger = false;
     }
 }
--- Assets/Scripts/PistolScript.cs
+++ Assets/Scripts/PistolScript.cs
@@ -7,11 +7,17 @@ public class PistolScript : MonoBehaviour
     string currentFireButton = "";
     public GameObject firePoint;
     public GameObject bullet;
-    public float fireRate;
+    public GameObject currentBullet;
+
     public float bulletSpeed;
     public float aliveTime;
     public float numBullets;
     public float bulletSpread;
+
+    public float timeOut = 1f;
+    public float timeRemaining;
+    public bool canFire;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -21,9 +27,40 @@ public class PistolScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetAxis(currentFireButton) != 0)
+        if(currentFireButton != "")
         {
-            Instantiate(bullet, firePoint.transform.position, firePoint.transform.rotation);
+            if (Input.GetAxis(currentFireButton) != 0 && canFire)
+            {
+                if (numBullets == 1)
+                {
+                    firePoint.transform.localEulerAngles = new Vector3(0, 0, 0);
+                    currentBullet = Instantiate(bullet, firePoint.transform.position, firePoint.transform.rotation);
+                    currentBullet.GetComponent<BulletScript>().speed = bulletSpeed;
+                    currentBullet.GetComponent<BulletScript>().aliveTime = aliveTime;
+                }
+                else
+                {
+                    firePoint.transform.localEulerAngles = new Vector3(0, 0, (bulletSpread / 2));
+                    for (int i = 0; i < numBullets; i++)
+                    {
+                        Debug.Log(firePoint.transform.localEulerAngles.z);
+                        currentBullet = Instantiate(bullet, firePoint.transform.position, firePoint.transform.rotation);
+                        firePoint.transform.localEulerAngles = new Vector3(0, 0, firePoint.transform.localEulerAngles.z - (bulletSpread / (numBullets - 1)));
+                        currentBullet.GetComponent<BulletScript>().speed = bulletSpeed;
+                        currentBullet.GetComponent<BulletScript>().aliveTime = aliveTime;
+                    }
+                }
+                canFire = false;
+                timeRemaining = timeOut;
+            }
+        }
+        if (!canFire)
+        {
+            timeRemaining -= Time.deltaTime;
+            if(timeRemaining <= 0)
+            {
+                canFire = true;
+            }
         }
     }
 }
