92cb4d43211bf7afcc39a111e875f181f74c7894
Tucker <tucker.beard@mymail.champlain.edu>
2025-11-10 13:37:59 -0500

    random commit

--- Assets/Scripts/GunScript.cs
+++ Assets/Scripts/GunScript.cs
@@ -43,7 +43,6 @@ public class GunScript : MonoBehaviour
                     firePoint.transform.localEulerAngles = new Vector3(0, 0, (bulletSpread / 2));
                     for (int i = 0; i < numBullets; i++)
                     {
-                        Debug.Log(firePoint.transform.localEulerAngles.z);
                         currentBullet = Instantiate(bullet, firePoint.transform.position, firePoint.transform.rotation);
                         firePoint.transform.localEulerAngles = new Vector3(0, 0, firePoint.transform.localEulerAngles.z - (bulletSpread / (numBullets - 1)));
                         currentBullet.GetComponent<BulletScript>().speed = bulletSpeed;
